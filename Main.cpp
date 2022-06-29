75591f96825eb5364665038c3b7943d40894f73c
Sandesh Jangir <sandeshatgithub@gmail.com>
2022-06-29 13:02:53 +0530

    Created Input Interface

--- Main.cpp
+++ Main.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 
 int main(){
+    int input;
+    cout << "Celcius to Fahrenite/Kelvin Converter" <<endl;
+    cout << "Please select any one conversion method via their sno." <<endl;
+    cout << "1. Celcius to Kelvin" <<endl  << "2. Celcius to Fahrenite" <<endl;
+    cin >> input;
     return 0;
 }
