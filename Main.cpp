da9de6e097f4572b4ce55cc80632bf306c8cc35d
Sandesh Jangir <sandeshatgithub@gmail.com>
2022-06-29 13:13:48 +0530

    Created Condional Pipelines

--- Main.cpp
+++ Main.cpp
@@ -7,5 +7,20 @@ int main(){
     cout << "Please select any one conversion method via their sno." <<endl;
     cout << "1. Celcius to Kelvin" <<endl  << "2. Celcius to Fahrenite" <<endl;
     cin >> input;
+
+    // Conditionals
+    if (input == 1)
+    {
+        cout << "You have choosed to convert Celcius to Kelvin" << endl;
+    }
+    if (input == 2)
+    {
+        cout << "You have choosed to convert Celcius to Fahrenite" << endl;
+    }
+    else
+    {
+        cout << "Please Enter a Valid sno." << endl;
+    }
+    
     return 0;
 }
