0906915c071d574ceda5fc01715733fdfd53dd1e
Sandesh Jangir <sandeshatgithub@gmail.com>
2022-07-02 13:14:20 +0530

    Added function to convert Kelvin to Fahrenite

--- Main.cpp
+++ Main.cpp
@@ -4,11 +4,12 @@ using namespace std;
 int main(){
     float c_to_k(float);
     float c_to_f(float);
+    float k_to_f(float);
 
     int input;
     cout << "Celcius to Fahrenite/Kelvin Converter" <<endl;
     cout << "Please select any one conversion method via their sno." <<endl;
-    cout << "1. Celcius to Kelvin" <<endl  << "2. Celcius to Fahrenite" <<endl;
+    cout << "1. Celcius to Kelvin" <<endl  << "2. Celcius to Fahrenite" <<endl << "3. Kelvin to Fahrenite" <<endl;
     cin >> input;
 
     // Conditionals
@@ -26,6 +27,13 @@ int main(){
         cin >> cel;
         cout << "Temperature in Fahrenite is : " << c_to_f(cel);
     }
+    else if (input == 3)
+    {
+        float k;
+        cout << "Enter Temperature in Kelvin : ";
+        cin >> k;
+        cout << "Temperature in Fahrenite is : " << k_to_f(k);
+    }
     else
     {
         cout << "Please Enter a Valid sno." << endl;
@@ -40,5 +48,9 @@ float c_to_k(float temp){
 }
 float c_to_f(float temp){
     float result = (temp * 9/5) + 32;
+    return result;
+}
+float k_to_f(float temp){
+    float result = (temp-273.15) * 9/5 + 32;
     return result;
 }
